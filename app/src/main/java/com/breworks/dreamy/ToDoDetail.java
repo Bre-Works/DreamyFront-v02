a479fc50d664f606e8c361de8e75b8f5464597a1
arsianindita <puspita.parahita@gmail.com>
2014-10-27 10:13:06 +0700

    ToDo Details
    - add ToDoDetail.java
    - add tododetails_form.xml

---
+++ app/src/main/java/com/breworks/dreamy/ToDoDetail.java
@@ -0,0 +1,15 @@
+package com.breworks.dreamy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by arsianindita on 27-Oct-14.
+ */
+public class ToDoDetail extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tododetails_form);
+    }
+}
