e5a150e83a45f4c529691508bac3512849d0d80b
arsianindita <puspita.parahita@gmail.com>
2014-11-03 12:55:19 +0700

    ToDoDetail dateField and timeField
    change from datepicker and timepicker widget to EditText dateField and
    timeField (which later when clicked will open datepicker or timepicker
    dialog).
    
    the EditText field currently showing current data and time

--- app/src/main/java/com/breworks/dreamy/ToDoDetail.java
+++ app/src/main/java/com/breworks/dreamy/ToDoDetail.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TimePicker;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import static android.app.PendingIntent.getActivity;
@@ -24,16 +27,31 @@ public class ToDoDetail extends Activity{
     int month;
     int year;
     Calendar calendar;
+    int taskID;
+
+    EditText dateField;
+    EditText timeField;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tododetails_form);
+        dateField = (EditText) findViewById(R.id.dateField);
+        timeField = (EditText) findViewById(R.id.timeField);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm ");
 
         calendar = Calendar.getInstance();
         day = calendar.get(Calendar.DAY_OF_MONTH);
         month = calendar.get(Calendar.MONTH);
         year = calendar.get(Calendar.YEAR);
 
+       dateField.setText(dateFormat.format(calendar.getTime()));
+       timeField.setText(timeFormat.format(calendar.getTime()));
+    }
+
+    public void setTaskID(int taskID){
+        this.taskID = taskID;
     }
 
 
