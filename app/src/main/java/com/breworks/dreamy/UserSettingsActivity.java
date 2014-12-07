4f3c6e4dbf0a25270c41b4b4f28ee5b10bee4797
arsianindita <puspita.parahita@gmail.com>
2014-12-07 07:30:07 +0700

    New User Setting

--- app/src/main/java/com/breworks/dreamy/UserSettingsActivity.java
+++ app/src/main/java/com/breworks/dreamy/UserSettingsActivity.java
@@ -1,18 +1,31 @@
 package com.breworks.dreamy;
 
 /**
- * Created by Gregorius Adrian on 12/4/2014.
+ * Created by Gregorius Adrian on 12/3/2014.
  */
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.widget.EditText;
 
 public class UserSettingsActivity extends PreferenceActivity {
+
+    EditText firstNameForm, lastNameForm, oldPasswordForm, newPasswordForm, newPasswordConfForm;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.account_setting);
 
-        addPreferencesFromResource(R.xml.settings);
+        firstNameForm = (EditText) findViewById(R.id.setFirstName);
+        lastNameForm = (EditText) findViewById(R.id.setLastName);
+        oldPasswordForm = (EditText) findViewById(R.id.setOldPass);
+        newPasswordForm =(EditText) findViewById(R.id.setNewPass);
+        oldPasswordForm = (EditText) findViewById(R.id.setNewPassConf);
+
+
+
+
 
     }
 }
