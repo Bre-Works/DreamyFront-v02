0c057461ee1be28b7ba688112848f9457787d5f1
Risdani Agung <eater.luck@gmail.com>
2014-12-08 15:11:44 +0700

    AA

--- app/src/main/java/com/breworks/dreamy/DreamyWidgetProvider.java
+++ app/src/main/java/com/breworks/dreamy/DreamyWidgetProvider.java
@@ -14,7 +14,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.RemoteViews;
+import android.widget.TextView;
 
 import com.breworks.dreamy.model.Dream;
 import com.breworks.dreamy.model.dreamyAccount;
@@ -44,16 +46,20 @@ public class DreamyWidgetProvider extends AppWidgetProvider {
             //String[] omom = {"Nama Saya Om", "Om nama saya", "Bukan Om Om"};
             String[] dreamSet = new String[dreams.size()];
             int inc = 0;
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.widget_layout);
+
             for(Dream om : dreams){
                 dreamSet[inc] = om.getName().toString();
+                TextView tv = new TextView(context);
+                tv.setText(om.getName().toString());
+
+                Log.w("WidgetExample", String.valueOf(dreamSet[number]));
+                // Set the textz
+                remoteViews.setTextViewText(R.id.update, dreamSet[number]);
                 inc++;
             }
 
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-                    R.layout.widget_layout);
-            Log.w("WidgetExample", String.valueOf(dreamSet[number]));
-            // Set the text
-            remoteViews.setTextViewText(R.id.update, dreamSet[number]);
 
             // Register an onClickListener
             Intent intent = new Intent(context, DreamyWidgetProvider.class);
